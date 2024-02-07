9451d9a224fb065f7b95770a030866307ff34d5b
PedroVidalDev <pedrohvidals@gmail.com>
2024-02-07 04:08:24 -0300

    [feat] - creating delete user function in api

--- src/main/java/com/pedro/picpay/application/UserController.java
+++ src/main/java/com/pedro/picpay/application/UserController.java
@@ -1,29 +1,39 @@
 package com.pedro.picpay.application;
 
-import com.pedro.picpay.application.dtos.UsuarioInputDTO;
-import com.pedro.picpay.domain.UsuarioService;
+import com.pedro.picpay.application.dtos.UserCreateInputDTO;
+import com.pedro.picpay.domain.UserService;
+import com.pedro.picpay.infrastructure.repositories.UserRepository;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
+import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/users")
-public class UsuarioController {
+public class UserController {
 
     @Autowired
-    private UsuarioService service;
+    private UserService service;
+
+    @Autowired
+    private UserRepository repository;
 
     @PostMapping
     @Transactional
-    public ResponseEntity createUser(@RequestBody @Valid UsuarioInputDTO data){
+    public ResponseEntity createUser(@RequestBody @Valid UserCreateInputDTO data){
         var user = service.verifyUserCreation(data);
 
         return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping("/{id}")
+    @Transactional
+    public ResponseEntity deleteUser(@PathVariable String id){
+        repository.deleteById(Long.parseLong(id));
+
+        return ResponseEntity.noContent().build();
     }
 
 }
