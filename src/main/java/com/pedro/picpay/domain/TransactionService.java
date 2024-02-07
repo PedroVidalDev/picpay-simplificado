763af94bb2c1a246c6798d1f466e94a2d449a918
PedroVidalDev <pedrohvidals@gmail.com>
2024-02-07 04:40:16 -0300

    [feat] - creating validations to make the transaction in the best way

--- src/main/java/com/pedro/picpay/domain/TransactionService.java
+++ src/main/java/com/pedro/picpay/domain/TransactionService.java
@@ -1,11 +1,14 @@
 package com.pedro.picpay.domain;
 
 import com.pedro.picpay.application.dtos.transaction.TransactionInputDTO;
+import com.pedro.picpay.application.dtos.transaction.TransactionOutputDTO;
+import com.pedro.picpay.infrastructure.entities.Transaction;
 import com.pedro.picpay.infrastructure.entities.User;
 import com.pedro.picpay.infrastructure.repositories.TransactionRepository;
 import com.pedro.picpay.infrastructure.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.interceptor.TransactionAttribute;
 
 @Service
 public class TransactionService {
@@ -15,10 +18,19 @@ public class TransactionService {
 
     @Autowired
     private TransactionRepository transactionRepository;
-    public void verifyTransaction(TransactionInputDTO data){
-        var payer = userRepository.findById(data.payer());
-        var payee = userRepository.findById(data.payee());
+    public TransactionOutputDTO verifyTransaction(TransactionInputDTO data){
+        var payer = userRepository.getReferenceById(data.payer());
+        var payee = userRepository.getReferenceById(data.payee());
 
+        if(payer.getValue() < data.value()){
+            throw new RuntimeException("pagador nao tem dinheiro suficiente");
+        } else{
+            payee.actualizeValue(data.value());
+            payer.actualizeValue(data.value() * -1);
 
+            Transaction transaction = new Transaction(null, payer, payee, data.value());
+
+            return new TransactionOutputDTO(transaction);
+        }
     }
 }
